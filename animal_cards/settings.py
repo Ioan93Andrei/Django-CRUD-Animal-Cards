71688319bfe113ee76171d1a591f187dc9e8a900
Ioan Andrei <ivan.andrei93@mail.ru>
2023-02-12 22:55:57 +0100

    added the Django login and logout pages

--- animal_cards/settings.py
+++ animal_cards/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'animal_cards.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+LOGIN_REDIRECT_URL = '/'
